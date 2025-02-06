64a46c25aa66efb8437df62f66aa4ab154c2a4a9
romiyusnandar <yusromi04@gmail.com>
2025-02-06 10:08:27 +0000

    Ryu-UI Org
    Change-Id: Icd12dd22b0ff2ae4efbae7817fbb01e016c068e7
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -454,8 +454,8 @@ endif
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(CUSTOM_BUILD),)
-include vendor/aosp/config/BoardConfigCustom.mk
+ifneq ($(RYU_BUILD),)
+include vendor/ryu/config/BoardConfigRyu.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1290,7 +1290,7 @@ include $(BUILD_SYSTEM)/sysprop_config.mk
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(CUSTOM_BUILD),)
+ifneq ($(RYU_BUILD),)
 ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -515,7 +515,7 @@ PRODUCT_PACKAGES_DEBUG := \
     unwind_reg_info \
     unwind_symbols \
 
-ifeq ($(CUSTOM_BUILD),)
+ifeq ($(RYU_BUILD),)
 PRODUCT_PACKAGES_DEBUG += \
     su
 endif
